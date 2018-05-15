4a0843295df751a0c079080c1cca325062ac8e94
Anshuman Dewangan <anshuman64@gmail.com>
2018-05-15 12:16:24 -0700

    Fix token refresh bug

--- src/screens/loading_screen/loading_screen.js
+++ src/screens/loading_screen/loading_screen.js
@@ -68,7 +68,9 @@ class LoadingScreen extends React.PureComponent {
             }
           })
           .catch((error) => {
-            defaultErrorAlert(error);
+            if (error.message != 'Token refresh was in progress') {
+              defaultErrorAlert(error);
+            }
           });
       } else {
         // console.log('No Firebase cookie found'); // Debug Test
